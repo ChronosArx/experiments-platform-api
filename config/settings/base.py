0612648a76f07e84f42aa0f71cafb771f68fd2f3
David <david.arx.dev@gmail.com>
2026-06-20 02:12:30 -0600

    feat(settings): add MEDIA_ROOT configuration for media file storage

--- config/settings/base.py
+++ config/settings/base.py
@@ -109,6 +109,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
